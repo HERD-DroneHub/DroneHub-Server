fb381f4f743854baa965686bdf1b7b25293585a9
mhoang17 <mtoh@cs.aau.dk>
2026-01-29 11:29:08 +0100

    Update README.md

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # DroneHub-Server
 This repository is for the DroneHub server that connects a drone with a controller running with a compatible app and the DroneHub Website.
+
+# Setup
+To install all necessary packages please run the command:
+
+```bash
+npm install
+```
+
+To run the server, run the command:
+
+```bash
+npm start
+```
+
+The application is running correctly if it prints out "Listening on 1337".
+
+# Usage
+In order to connect a drone to the server, it is important that you know the IP address of the network as it is necessary to know the specific IP to establish a connection with sockets. You can use the command:
+
+```bash
+ipconfig getifaddr en0
+```
+
+If you want to use your custom software to connect a drone to the server, it is important that the data sent through the sockets are consistent with the format used in the server.
+Please look in the ``models`` folder to see the correct data format for the different data types.
+
--- src/utils/socket-names.ts
+++ src/utils/socket-names.ts
@@ -1,9 +1,12 @@
 export const POLYGON = "polygon";
 export const POLYGON_DELETE = "polygon-delete";
 export const ERROR = "errors";
-export const EVENT = "events";
-export const DRONE = "drones";
+
+export const EVENT = "events"; // The channel for drone events. Events are messages sent by drones either manually or automatically.
+
+export const DRONE = "drones"; // The channel for drone information updates. Every time a drone updates, it will be broadcasted to this channel.
 export const DRONE_DISCONNECT = "drone-disconnect";
+
 export const DRONE_REQUEST_GOTO_LOCATION = "drone-request-goto";
 export const DRONE_GOTO_LOCATION = "drone-goto";
 export const DRONE_REQUEST_RTH = "drone-request-rth";
@@ -15,6 +18,7 @@ export const DRONE_UNSET_LEADER = "drone-unset-leader";
 export const MISSION = "missions";
 export const DRONE_REQUEST_WAYPOINT_MISSION = "drone-request-waypoint";
 export const DRONE_WAYPOINT_MISSION = "drone-waypoint";
+
 export const DRONE_VIDEO_START = "drone-video-start";
 export const DRONE_VIDEO_DATA = "drone-video-data";
 export const ADD_DRONE_TO_VIDEOSCREEN = "add-drone-to-videoscreen";
